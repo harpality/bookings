c93d0895ef2ead86b412c1c1faa1ddd81f247fe6
Harpal <harpalassi@gmail.com>
2019-07-27 18:17:48 -0500

    created navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="icon">
+        <i className="fab fa-black-tie" />
+      </div>
+      <div className="title">
+        <h1>Apartment Butler</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
