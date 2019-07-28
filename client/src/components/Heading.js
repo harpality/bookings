cf459639b4ab3daa4c0cc90d4e750708511f7de4
Harpal <harpalassi@gmail.com>
2019-07-28 17:42:13 -0500

    form complete

--- client/src/components/Heading.js
+++ client/src/components/Heading.js
@@ -11,7 +11,10 @@ class Heading extends Component {
     street: '',
     city: '',
     state: '',
-    zipcode: ''
+    zipcode: '',
+    bookingType: 'Dog Walk',
+    date: '',
+    time: ''
   };
 
   componentDidMount() {
@@ -43,7 +46,10 @@ class Heading extends Component {
       this.state.street,
       this.state.city,
       this.state.state,
-      this.state.zipcode
+      this.state.zipcode,
+      this.state.bookingType,
+      this.state.date,
+      this.state.time
     );
   };
 
@@ -55,59 +61,110 @@ class Heading extends Component {
         <Modal className="modal" isOpen={this.state.showModal}>
           <h2>Create booking</h2>
           <form onSubmit={this.handleFormSubmit}>
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              onChange={this.handleInputChange}
-              name="name"
-              value={this.state.name}
-            />
-            <label htmlFor="email">Email</label>
-            <input
-              type="text"
-              onChange={this.handleInputChange}
-              name="email"
-              value={this.state.email}
-            />
-            <label htmlFor="address">Street Address</label>
-            <input
-              type="text"
-              onChange={this.handleInputChange}
-              name="street"
-              value={this.state.street}
-            />
-            <label htmlFor="city">City</label>
-            <input
-              type="text"
-              onChange={this.handleInputChange}
-              name="city"
-              value={this.state.city}
-            />
-            <div className="stateZip">
-              <div className="labelOnTop">
-                <label htmlFor="state">State</label>
+            <div className="row">
+              <div className="column">
+                <label htmlFor="name">Name</label>
                 <input
                   type="text"
                   onChange={this.handleInputChange}
-                  name="state"
-                  value={this.state.state}
-                  className="state"
+                  name="name"
+                  value={this.state.name}
+                  className="name"
                 />
               </div>
-              <div className="labelOnTop">
-                <label htmlFor="zipcode">Zip code</label>
+              <div className="column">
+                <label htmlFor="bookingType">Booking type</label>
+                <select
+                  name="bookingType"
+                  value={this.state.bookingType}
+                  onChange={this.handleInputChange}>
+                  <option value="Dog Walk">Dog Walk</option>
+                  <option value="Housekeeping">Housekeeping</option>
+                </select>
+              </div>
+            </div>
+            <div className="row">
+              <div className="column">
+                <label htmlFor="email">Email</label>
                 <input
                   type="text"
                   onChange={this.handleInputChange}
-                  name="zipcode"
-                  value={this.state.zipcode}
-                  className="zipcode"
+                  name="email"
+                  value={this.state.email}
+                />
+              </div>
+              <div className="column">
+                <label htmlFor="date">Booking date</label>
+                <input
+                  type="text"
+                  onChange={this.handleInputChange}
+                  name="date"
+                  value={this.state.date}
+                  className="date"
                 />
               </div>
             </div>
-            <input type="submit" value="Create booking" />
+            <div className="row">
+              <div className="column">
+                <label htmlFor="address">Street Address</label>
+                <input
+                  type="text"
+                  onChange={this.handleInputChange}
+                  name="street"
+                  value={this.state.street}
+                />
+              </div>
+              <div className="column">
+                <label htmlFor="date">Booking time</label>
+                <input
+                  type="text"
+                  onChange={this.handleInputChange}
+                  name="time"
+                  value={this.state.time}
+                />
+              </div>
+            </div>
+            <div className="column">
+              <label htmlFor="city">City</label>
+              <input
+                type="text"
+                onChange={this.handleInputChange}
+                name="city"
+                value={this.state.city}
+                className="city"
+              />
+            </div>
+            <div className="stateZip">
+              <div className="row">
+                <div className="column">
+                  <label htmlFor="state">State</label>
+                  <input
+                    type="text"
+                    onChange={this.handleInputChange}
+                    name="state"
+                    value={this.state.state}
+                    className="state"
+                  />
+                </div>
+                <div className="column">
+                  <label className="zipcode" htmlFor="zipcode">
+                    Zip code
+                  </label>
+                  <input
+                    type="text"
+                    onChange={this.handleInputChange}
+                    name="zipcode"
+                    value={this.state.zipcode}
+                    className="zipcode"
+                  />
+                </div>
+              </div>
+            </div>
+            <button type="submit" value="Create booking">
+              Create booking
+            </button>
           </form>
-          <button onClick={this.handleCloseModal}>Close Modal</button>
+          <button onClick={this.handleCloseModal}>Close</button>
         </Modal>
       </div>
     );
